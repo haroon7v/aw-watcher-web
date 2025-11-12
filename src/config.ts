fa54f6d13bebff7b62360aa3e562cd5a4015b950
Haroon Rasheed <haroon.rasheed@7vals.com>
2025-11-12 12:27:01 +0500

    manifest v3 update, refs#43946

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
   isDevelopment: import.meta.env.DEV,
-  requireConsent: import.meta.env.VITE_TARGET_BROWSER === 'firefox',
+  requireConsent: false,
   assetsonarServer: {
     protocol: import.meta.env.DEV ? 'http' : 'https',
     host: import.meta.env.DEV ? 'lvh.me:3000' : 'assetsonar.com',
